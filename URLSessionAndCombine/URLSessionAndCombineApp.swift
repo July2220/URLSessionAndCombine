5b39ff7df8c3df10363c764039723198b503e52b
July Tao <daymatters@protonmail.com>
2023-03-23 18:37:55 +0800

    Initial Commit

---
+++ URLSessionAndCombine/URLSessionAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionAndCombineApp.swift
+//  URLSessionAndCombine
+//
+//  Created by july on 2023/3/23.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
